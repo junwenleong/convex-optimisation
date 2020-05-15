328abe56f2280b0f2bb2e9c793c265158f4396c4
junwenleong <58838483+junwenleong@users.noreply.github.com>
2020-05-15 19:52:00 +0800

    Create accelproxgrad.R

---
+++ Accelerated Proximal Gradient/accelproxgrad.R
@@ -0,0 +1,63 @@
+library(gradDescent)
+library(pracma)
+library(rje)
+library(R.matlab)
+
+data <- readMat("APGdata.mat") #load Matlab data file
+
+delf <- function(w){ #method to compose gradient vector of objective function
+  grad <- 0
+  for (i in 1:3065){
+    yi <- data$ytrain[i,]
+    xi <- data$Xtrain[i,]
+    expo <- exp(-yi*w%*%xi)
+    expo2 <- expo[1,1]
+    p1 <- 1 + expo
+    pa <- p1[1,1]
+    p2 <- -(yi*xi)
+    p3 <- (expo2/pa)*p2
+    grad <- grad + p3
+  }
+  return (grad)
+}
+
+originalf <- function(w){
+  outf <- 0
+  for (i in 1:3065){
+    xi <- data$Xtrain[i,]
+    yi <- data$ytrain[i,]
+    p1 <- 1 + exp(-yi * w% * %xi)
+    p2 <- log(p1)
+    outf <- outf + p2
+  }
+  return (outf[1,1])
+}
+
+accproxgrad <- function(initial, L, e, iterbound = 10000) {  # accelerated proximal gradient projection
+  i <- 1
+  oldx <- initial
+  oldt <- 1
+  newt <- 1
+  beta <- (oldt - 1)/newt
+  X <- initial
+  delfvalue <- delf(X)
+  tempvalue <- 0
+  newx <- sign(X - (1/L)*delfvalue) * pmax(Wvec, abs(X - (1/L)*delfvalue) - (0.05/L))
+  while (norm(newx - oldx, type = "2") > e && i < iterbound){
+    oldt <- newt
+    newt <- 0.5*(1+sqrt(1+4*(oldt^2)))
+    beta <- (oldt-1)/newt
+    X <- newx + beta * (newx-oldx)
+    delfvalue <- delf(X)
+    oldx <- newx
+    newx <- sign(X - (1/L)*delfvalue)*pmax(Wvec, abs(X - (1/L)*delfvalue) - (0.05/L))
+    i <- i + 1
+  }
+  return (newx)
+}
+
+# Lcon <- 0.5 * (norm(data$Xtrain, type = c("F")))^2
+# Wvec <- rep(0, 57)
+# accproxgrad(Wvec, Lcon, 0.001)
+
+
